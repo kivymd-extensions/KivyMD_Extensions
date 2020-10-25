7750bad663a79065e625a6712f3c681d1b9006ce
macbookair <kivydevelopment@gmail.com>
2020-10-26 00:36:52 +0300

    Update `setup.py`

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
         name='KivyMD Components',
         version='0.1.4',
         license='MIT',
+        author='Kivy Team',
+        description='Script for installing additional components for the KivyMD library',
         packages=['components'],
         scripts=['bin/components', 'bin/components.bat'],
         install_requires=['requests'],
