d4627b04f9d6dcd999d65ab71989152df7a7c95a
Yuri Ivanov <kivydevelopment@gmail.com>
2020-10-28 13:44:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,5 @@
 #!python
 
-"""Bootstrap setuptools installation
-
-If you want to use setuptools in your package's setup.py, just include this
-file in the same directory with it, and add this to the top of your setup.py::
-
-    from ez_setup import use_setuptools
-    use_setuptools()
-
-If you want to require a specific version of setuptools, set a download
-mirror, or use an alternate download directory, you can do so by supplying
-the appropriate options to ``use_setuptools()``.
-
-This file can also be run as a script to install or upgrade setuptools.
-"""
-
 import os
 import shutil
 import sys
